9a86adb821208dcf204fa7e9f67f9c6bf2f8aa21
jacobemerick <jpemeric@gmail.com>
2017-01-07 04:38:53 -0700

    Breaks up SecurityCheck into digestible pieces

--- src/SecurityCheck.php
+++ src/SecurityCheck.php
@@ -27,11 +27,29 @@ class SecurityCheck
     public function checkScheme(array $scheme)
     {
         if ($scheme['type'] === 'basic') {
-            $authHeader = $this->request->getHeader('Authorization');
-            $authHeader = explode(' ', $authHeader);
-            return ($authHeader[0] === 'Basic' && preg_match('/^[a-z0-9]+$/i', $authHeader[1]) === 1);
+            return $this->checkBasicScheme();
+        } elseif ($scheme['type'] === 'oauth') {
+            return $this->checkOAuthScheme($scheme);
         }
-        // todo oauth
         return false;
+    }
+
+    /**
+     * @return boolean
+     */
+    protected function checkBasicScheme()
+    {
+        $authHeader = $this->request->getHeader('Authorization');
+        $authHeader = explode(' ', $authHeader);
+        return ($authHeader[0] === 'Basic' && preg_match('/^[a-z0-9]+$/i', $authHeader[1]) === 1);
+    }
+
+    /**
+     * @param array $scheme
+     * @return boolean
+     */
+    protected function checkOauthScheme(array $scheme)
+    {
+        throw new \Exception('OAuth is not yet implemented');
     }
 }
