8a5c5c418e92a7a3441081aa141c9d0ff74fc31c
jacobemerick <jpemeric@gmail.com>
2017-01-14 03:48:30 -0700

    Adds tests around basic security header check

--- tests/unit/src/SecurityCheckTest.php
+++ tests/unit/src/SecurityCheckTest.php
@@ -4,6 +4,7 @@ namespace AvalancheDevelopment\SwaggerValidationMiddleware;
 
 use PHPUnit_Framework_TestCase;
 use Psr\Http\Message\RequestInterface;
+use ReflectionClass;
 
 class SecurityCheckTest extends PHPUnit_Framework_TestCase
 {
@@ -84,8 +85,79 @@ class SecurityCheckTest extends PHPUnit_Framework_TestCase
         $this->assertFalse($result);
     }
 
-    public function testCheckBasicSchemePullsAuthorizationFromHeader() {}
-    public function testCheckBasicSchemeReturnsFalseIfNotBasicAuth() {}
-    public function testCheckBasicSchemeReturnsFalseIfInvalidAuth() {}
-    public function testCheckBasicSchemeReturnsTrueIfValidAuthHeader() {}
+    public function testCheckBasicSchemePullsAuthorizationFromHeader()
+    {
+        $mockRequest = $this->createMock(RequestInterface::class);
+        $mockRequest->expects($this->once())
+            ->method('getHeader')
+            ->with('Authorization')
+            ->willReturn('Basic Header');
+
+        $reflectedSecurityCheck = new ReflectionClass(SecurityCheck::class);
+        $reflectedCheckBasicScheme = $reflectedSecurityCheck->getMethod('checkBasicScheme');
+        $reflectedCheckBasicScheme->setAccessible(true);
+
+        $securityCheck = $this->getMockBuilder(SecurityCheck::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $reflectedCheckBasicScheme->invokeArgs($securityCheck, [ $mockRequest ]);
+    }
+
+    public function testCheckBasicSchemeReturnsFalseIfNotBasicAuth()
+    {
+        $mockRequest = $this->createMock(RequestInterface::class);
+        $mockRequest->method('getHeader')
+            ->willReturn('Bearer Header');
+
+        $reflectedSecurityCheck = new ReflectionClass(SecurityCheck::class);
+        $reflectedCheckBasicScheme = $reflectedSecurityCheck->getMethod('checkBasicScheme');
+        $reflectedCheckBasicScheme->setAccessible(true);
+
+        $securityCheck = $this->getMockBuilder(SecurityCheck::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $result = $reflectedCheckBasicScheme->invokeArgs($securityCheck, [ $mockRequest ]);
+
+        $this->assertFalse($result);
+    }
+
+    public function testCheckBasicSchemeReturnsFalseIfInvalidAuth()
+    {
+        $mockRequest = $this->createMock(RequestInterface::class);
+        $mockRequest->method('getHeader')
+            ->willReturn('Basic ');
+
+        $reflectedSecurityCheck = new ReflectionClass(SecurityCheck::class);
+        $reflectedCheckBasicScheme = $reflectedSecurityCheck->getMethod('checkBasicScheme');
+        $reflectedCheckBasicScheme->setAccessible(true);
+
+        $securityCheck = $this->getMockBuilder(SecurityCheck::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $result = $reflectedCheckBasicScheme->invokeArgs($securityCheck, [ $mockRequest ]);
+
+        $this->assertFalse($result);
+    }
+
+    public function testCheckBasicSchemeReturnsTrueIfValidAuthHeader()
+    {
+        $mockRequest = $this->createMock(RequestInterface::class);
+        $mockRequest->method('getHeader')
+            ->willReturn('Basic Header');
+
+        $reflectedSecurityCheck = new ReflectionClass(SecurityCheck::class);
+        $reflectedCheckBasicScheme = $reflectedSecurityCheck->getMethod('checkBasicScheme');
+        $reflectedCheckBasicScheme->setAccessible(true);
+
+        $securityCheck = $this->getMockBuilder(SecurityCheck::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $result = $reflectedCheckBasicScheme->invokeArgs($securityCheck, [ $mockRequest ]);
+
+        $this->assertTrue($result);
+    }
 }
