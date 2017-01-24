7ff69db6e596e00bd5313d60580d2c9afaceb461
jacobemerick <jpemeric@gmail.com>
2017-01-24 04:51:36 -0700

    Start of parameterCheck breakdown

--- src/ParameterCheck.php
+++ src/ParameterCheck.php
@@ -14,6 +14,95 @@ class ParameterCheck
      */
     public function checkParams(RequestInterface $request, array $params)
     {
+        $self = $this;
+        $isValid = array_reduce(
+            $params,
+            function ($result, $param) use ($self, $request) {
+                return ($self->checkParam($request, $param) && $result);
+            },
+            true
+        );
+        return $isValid;
+    }
+
+    /**
+     * @param RequestInterface $request
+     * @param array $param
+     * @return boolean
+     */
+    protected function checkParam(RequestInterface $request, array $param)
+    {
+        switch ($param['in']) {
+            case 'body':
+                $result = $this->checkBodyParam($request, $param);
+                break;
+            case 'formData':
+                $result = $this->checkFormParam($request, $param);
+                break;
+            case 'header':
+                $result = $this->checkHeaderParam($request, $param);
+                break;
+            case 'path':
+                $result = $this->checkPathParam($request, $param);
+                break;
+            case 'query':
+                $result = $this->checkQueryParam($request, $param);
+                break;
+            default:
+                throw new \Exception('Invalid location set for parameter');
+                break;
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param RequestInterface $request
+     * @param array $param
+     * @return boolean
+     */
+    protected function checkBodyParam(RequestInterface $request)
+    {
+        return true;
+    }
+
+    /**
+     * @param RequestInterface $request
+     * @param array $param
+     * @return boolean
+     */
+    protected function checkFormParam(RequestInterface $request)
+    {
+        return true;
+    }
+
+    /**
+     * @param RequestInterface $request
+     * @param array $param
+     * @return boolean
+     */
+    protected function checkHeaderParam(RequestInterface $request)
+    {
+        return true;
+    }
+
+    /**
+     * @param RequestInterface $request
+     * @param array $param
+     * @return boolean
+     */
+    protected function checkPathParam(RequestInterface $request)
+    {
+        return true;
+    }
+
+    /**
+     * @param RequestInterface $request
+     * @param array $param
+     * @return boolean
+     */
+    protected function checkQueryParam(RequestInterface $request)
+    {
         return true;
     }
 }
--- tests/unit/src/ParameterCheckTest.php
+++ tests/unit/src/ParameterCheckTest.php
@@ -3,7 +3,82 @@
 namespace AvalancheDevelopment\SwaggerValidationMiddleware;
 
 use PHPUnit_Framework_TestCase;
+use Psr\Http\Message\RequestInterface;
 
 class ParameterCheckTest extends PHPUnit_Framework_TestCase
 {
+
+    public function testCheckParamsPassesEachParamAgainstParamCheck()
+    {
+        $mockParams = [
+            [ 'one' ],
+            [ 'two' ],
+        ];
+
+        $mockRequest = $this->createMock(RequestInterface::class);
+
+        $parameterCheck = $this->getMockBuilder(ParameterCheck::class)
+            ->disableOriginalConstructor()
+            ->setMethods([ 'checkParam' ])
+            ->getMock();
+        $parameterCheck->expects($this->exactly(count($mockParams)))
+            ->method('checkParam')
+            ->withConsecutive(
+                [
+                    $mockRequest,
+                    $mockParams[0],
+                ],
+                [
+                    $mockRequest,
+                    $mockParams[1],
+                ]
+            )
+            ->willReturn(true);
+
+        $parameterCheck->checkParams($mockRequest, $mockParams);
+    }
+
+    public function testCheckParamsReturnsTrueIfAllParamsAreValid()
+    {
+        $mockParams = [
+            [ 'valid' ],
+            [ 'valid' ],
+        ];
+
+        $mockRequest = $this->createMock(RequestInterface::class);
+
+        $parameterCheck = $this->getMockBuilder(ParameterCheck::class)
+            ->disableOriginalConstructor()
+            ->setMethods([ 'checkParam' ])
+            ->getMock();
+        $parameterCheck->method('checkParam')
+            ->willReturn(true);
+
+        $result = $parameterCheck->checkParams($mockRequest, $mockParams);
+
+        $this->assertTrue($result);
+    }
+
+    public function testCheckParamsReturnsFalseIfOneParamReturnsFalse()
+    {
+        $mockParams = [
+            [ 'valid' ],
+            [ 'invalid' ],
+        ];
+
+        $mockRequest = $this->createMock(RequestInterface::class);
+
+        $parameterCheck = $this->getMockBuilder(ParameterCheck::class)
+            ->disableOriginalConstructor()
+            ->setMethods([ 'checkParam' ])
+            ->getMock();
+        $parameterCheck->method('checkParam')
+            ->will($this->returnCallback(function ($mockRequest, $param) {
+                return current($param) === 'valid';
+            }));
+
+        $result = $parameterCheck->checkParams($mockRequest, $mockParams);
+
+        $this->assertFalse($result);
+    }
 }
